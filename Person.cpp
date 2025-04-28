6de7e0d42b4e8ef91e656b54cf6c6e4c49d607f6
allie <54782727+flimsychip@users.noreply.github.com>
2025-04-28 13:22:05 -0700

    Update Person.cpp

--- Person.cpp
+++ Person.cpp
@@ -1,2 +1,17 @@
-// test thru vscode
-// hello test
+#include "Person.h"
+
+string Person::getName() {
+    return this->name;
+}
+
+int Person::getAge() {
+    return this->age;
+}
+
+void Person::setName(string newName) {
+    this->name = newName;
+}
+
+void Person::setAge(int newAge) {
+    this->age = newAge;
+}
