c5f69121d878c438d7d74e1f816148115aa261ec
Milo <milopesce@gmail.com>
2025-04-28 13:47:40 -0700

    Filled out Exhibit.h

--- Exhibit.h
+++ Exhibit.h
@@ -1,1 +1,19 @@
-//test milo
+#ifndef EXHIBIT_H
+#define EXHIBIT_H
+
+#include "Animal.h"
+#include "Employee.h"
+#include "LinkedList.h"
+struct Exhibit {
+    public:
+        void addAnimal(Animal animal);
+        Animal rmAnimal();
+        void addCaretaker(Employee caretaker);
+        Employee rmCaretaker();
+
+    private:
+        LinkedList<Animal> animals;
+        LinkedList<Employee> employee;
+};
+
+#endif
