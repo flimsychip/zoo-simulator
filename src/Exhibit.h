acbc67408b54633c4d461cd8cf076e1f33de0710
Milo <milopesce@gmail.com>
2025-05-06 18:49:12 -0700

    Exhibit - Rule of three, name, and cost all added

--- src/Exhibit.h
+++ src/Exhibit.h
@@ -8,17 +8,36 @@
 struct Exhibit {
     public:
         Exhibit();
-        // FIXME: missing rule of three
-        
+        Exhibit(
+            double dailyCost, 
+            std::string name = "N/A", 
+            LinkedList<Animal>* animals = nullptr,
+            LinkedList<Employee>* employees = nullptr
+        );
+        Exhibit(const Exhibit& copyExhibit);       // Copy Constructor
+
         ~Exhibit();
+        void clear();
 
         void addAnimal(const Animal& animal);
         bool rmAnimal(const Animal& animal);
         void addCaretaker(const Employee& caretaker);
         bool rmCaretaker(const Employee& caretaker);
-        double getDailyCost() const; 
+
+        // OPERATORS
+        Exhibit &operator=(const Exhibit &rhs);     // Assignment operator
+
+        // GETTERS
+        double getDailyCost() const;
+        std::string getName() const;
+
+        // SETTERS
+        void setDailyCost(double newCost);
+        void setName(std::string newName);
 
     private:
+        double dailyCost;
+        std::string name;
         LinkedList<Animal>* animals;
         LinkedList<Employee>* employees;
 };
