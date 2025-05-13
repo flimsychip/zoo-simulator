71fc8262aa982106b803b06e453ff1668cd1389b
murlokian <rosarita.melton18@gmail.com>
2025-05-13 16:25:17 -0700

    added unitTesting.h -- still incomplete

--- src/unitTesting.cpp
+++ src/unitTesting.cpp
@@ -1,55 +1,63 @@
-// #include "Animal.h" 
-// #include "Customer.h" 
-// #include "Zoo.h" 
-// #include <iostream> 
-// #include <iomanip> // make output pretty 
-// #include <fstream>
+#include "Zoo.h" 
+#include "unitTesting.h"
 
-// using namespace std; 
+#include <iostream> 
+#include <iomanip>
+#include <fstream>
+#include <string>
 
-// enum ZooInfo {QUIT, EXHIBITS, CUSTOMERS, EMPLOYEES, END_INFO};
+using namespace std; 
 
-// void welcomeMsg();
-// void writeTestOutputToFile(const string& filename);
+enum ZooInfo {QUIT, EXHIBITS, CUSTOMERS, EMPLOYEES, END_INFO};
 
-// int main() {     
-//     cout << "Testing..." << endl;
+//void welcomeMsg();
+//void writeTestOutputToFile(const string& filename);
 
-//     welcomeMsg();
+void testingPriv() {     
+    cout << "Testing..." << endl;
 
-//     Zoo* zoo = new Zoo();
-//     // Exhibit* testCage = new Exhibit;
+    //welcomeMsg();
 
-//     // Animal* animal1 = new Animal();     
-//     // cout << animal1->getSpecies() << endl;
-//     // Animal* animal2 = new Animal(Chimpanzee);   
-//     // Animal* animal3 = new Animal(Hamster);      
+    Zoo* zoo = new Zoo();
+    Exhibit* testCage = new Exhibit;
+
+    Animal* animal1 = new Animal();     
+    cout << animal1->getSpecies() << endl;
+    Animal* animal2 = new Animal(Chimpanzee);   
+    Animal* animal3 = new Animal(Hamster);      
          
-//     // testCage->addAnimal(*animal1);
-//     // testCage->addAnimal(*animal2);
-//     // testCage->addAnimal(*animal3);
+    testCage->addAnimal(*animal1);
+    testCage->addAnimal(*animal2);
+    testCage->addAnimal(*animal3);
 
-//     // testCage->sort();
-//     // testCage->getEmployees()->print();
+    testCage->sort();
+    testCage->getEmployees()->print();
+    testCage->getAnimals()->print();
 
-//     //zoo->addExhibit(*testCage);
+    zoo->addExhibit(*testCage);
 
-//     Customer* customer = new Customer("Shakira");     
-//     cout << customer->getName() << endl;   
-//     Customer* customer2 = new Customer("Joe"); 
-//     Customer* customer3 = new Customer("Shmoe");   
+    Customer* customer = new Customer("Shakira");     
+    cout << customer->getName() << endl;   
+    Customer* customer2 = new Customer("Joe"); 
+    Customer* customer3 = new Customer("Shmoe");   
     
          
-//     cout << zoo->calcExpenses() << endl;     // Is this be pushing back deep copies?     
-//     zoo->addCustomer(*customer);   
-//     zoo->addCustomer(*customer2);   
-//     zoo->addCustomer(*customer3); 
+    cout << zoo->calcExpenses() << endl;     // Is this be pushing back deep copies?     
+    zoo->addCustomer(*customer);   
+    zoo->addCustomer(*customer2);   
+    zoo->addCustomer(*customer3); 
     
-//     zoo->sortCustomers();
-//     zoo->getCustomers()->print();
+    zoo->sortCustomers();
+    zoo->getCustomers()->print();
 
-//     delete zoo;
-//     return 0;
-// }
+    delete zoo;
+}
 
- 
+void testNodeBasic();
+void testLinkedListBasic ();
+void testNodeCompOperators ();
+void testLinkedListCompOperator ();
+//int getTestMenuChoice ();
+//void promptUnitTest();
+
+void outputList ();
---
+++ src/unitTesting.h
@@ -0,0 +1,16 @@
+#ifndef UNITTESTING_H 
+#define UNITTESTING_H
+
+#include <string>
+
+void testNodeBasic();
+void testLinkedListBasic();
+void testNodeCompOperators();
+void testLinkedListCompOperator();
+//int getTestMenuChoice();
+//void promptUnitTest();
+
+void outputList ();
+
+#endif
+// UNITTESTING_H
