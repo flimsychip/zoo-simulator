6b93bde920107489b329ac1bd7141dfd16d9f9f2
allie <54782727+flimsychip@users.noreply.github.com>
2025-05-15 23:16:36 -0700

    updated todo

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,10 @@
     TO DO: 
     - !!! filei for loading in objects !!!!!
     - fileo -- honestly we can just write everything we write in the console to a file :sob: make sure we include user inputs
-    - bug: can't modify data after pushing back (line 65)
+    - bug line 65: can't modify data after pushing back (not totally necessary, if not fixed just make sure we load in objects in the right order)
     - clean up unused functionality in all files (only one shows up in output, hit that first -- food inventory)
+        - adjust uml
+        - friend stuff
 */
 
 #include "Animal.h" 
